d11bca7a6fea020b4f09a91b74a300f810b06ee8
mvw <73003273+meesvw@users.noreply.github.com>
2022-04-02 23:23:31 +0200

    Update keltnerchannel.py
    20210101 -> 20220402 profit increase to 12933.42%
    
    Also removed unnecessary code

--- strategies/keltnerchannel.py
+++ strategies/keltnerchannel.py
@@ -9,9 +9,6 @@ import pandas_ta as pta
 import numpy as np  # noqa
 import pandas as pd  # noqa
 
-# These libs are for hyperopt
-from functools import reduce
-from freqtrade.strategy import (BooleanParameter, CategoricalParameter, DecimalParameter,IStrategy, IntParameter)
 
 class keltnerchannel(IStrategy):
     timeframe = "6h"
@@ -19,23 +16,9 @@ class keltnerchannel(IStrategy):
     stoploss = -0.254
     minimal_roi = {"0": 100}
 
-    plot_config = {
-        "main_plot": {
-            "kc_upperband" : {"color": "purple",'plotly': {'opacity': 0.4}},
-            "kc_middleband" : {"color": "blue"},
-            "kc_lowerband" : {"color": "purple",'plotly': {'opacity': 0.4}}
-        },
-        "subplots": {
-            "RSI": {
-                "rsi": {"color": "orange"},
-                "hline": {"color": "grey","plotly": {"opacity": 0.4}}
-            },
-        },
-    }
-
     def populate_indicators(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
         # Keltner Channel
-        keltner = qtpylib.keltner_channel(dataframe, window=20, atrs=1)
+        keltner = qtpylib.keltner_channel(dataframe, window=16, atrs=1)
         dataframe["kc_upperband"] = keltner["upper"]
         dataframe["kc_lowerband"] = keltner["lower"]
         dataframe["kc_middleband"] = keltner["mid"]
@@ -44,12 +27,8 @@ class keltnerchannel(IStrategy):
         dataframe["rsi"] = ta.RSI(dataframe, timeperiod=14)
 
         # Horizontal RSI line
-        hline = 55
-        dataframe['hline'] = hline
-
-        # Print stuff for debugging dataframe
-        # print(metadata)
-        # print(dataframe.tail(20))
+        dataframe['hline'] = 61
+        
         return dataframe
 
     def populate_buy_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
